fd949f60e84a5c712b266e05196b64f180d32460
fox <steven.fairclough@icloud.com>
2022-02-15 23:39:14 +0000

    First commit empty File

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+int FindSmallestNUmber(int number);
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+
+
     return 0;
+}
+
+
+int FindSmallestNUmber(int number){
+
+    int counter = 0;
+
+    for(int i = 1 ; i <= 20 ; i++ ){
+
+
+        if(number % i == 0 ){
+            counter++;
+
+        }else{
+             counter = 0;
+            return -1;
+        }
+
+        if(counter == 20){
+
+            cout<< number << endl;
+            return true;
+        }
+
+    return -1;
+    }
+
+
 }
